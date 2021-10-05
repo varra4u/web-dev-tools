3343066bde26535d41e8b7dd7de8fd35f7a69be2
Rajakrishna Reddy <varra@outlook.com>
2021-10-06 01:51:14 +0530

    Added build folder to run the app easily.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Browser based Dev Tools
+
+
+## Run the install to install all the dependencies.
+	npm install
+
+## Run the start to start the application in dev mode.
+	npm start
+	
+## Run the build using the below command to build the application for deployment:
+	npm run build
+
+## Run these to start the application in prod env:
+	npm install -g serve
+	serve -s build
---
+++ build-for-dbs/precache-manifest.0e86f64814357eb0cbac49334cf2deef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdc44fa64393c72a3db3",
+    "url": "/static/css/main.5ac54c85.chunk.css"
+  },
+  {
+    "revision": "cdc44fa64393c72a3db3",
+    "url": "/static/js/main.cdc44fa6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4449101b73c01920b7c2",
+    "url": "/static/css/2.262fbba9.chunk.css"
+  },
+  {
+    "revision": "4449101b73c01920b7c2",
+    "url": "/static/js/2.4449101b.chunk.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "16fc355a9294db2eea4f8153f335f27b",
+    "url": "/index.html"
+  }
+];
